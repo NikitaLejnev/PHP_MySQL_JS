eb3b93b96b3a006ed37038cca1d7f0ae9d53f779
NikitaLejnev <catassasin331@gmail.com>
2022-02-03 14:52:10 +0300

    Added 6-8.php

---
+++ Chapter06/6-8.php
@@ -0,0 +1,12 @@
+// Walking through an associative array using each and list
+// each() is removed from PHP8
+// <?php
+// $paper = array(
+//   'copier' => "Copier & Multipurpose",
+//   'inkjet' => "Inkjet Printer",
+//   'laser' => "Laser Printer",
+//   'photo' => "Photographic Paper"
+// );
+// while (list($item, $description) = each($paper)) {
+//   echo "$item: $description<br>";
+// }
