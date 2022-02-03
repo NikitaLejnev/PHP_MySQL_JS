47d7454e565acf68c5919d26f59bd006b840263f
NikitaLejnev <catassasin331@gmail.com>
2022-02-03 15:19:39 +0300

    Added 6-15.php

---
+++ Chapter06/6-14.php
@@ -0,0 +1,12 @@
+// Using the compact function
+
+<?php
+$fname =         "Doctor";
+$sname =         "Who";
+$planet =        "Gallifrey";
+$system =        "Gridlock";
+$constellation = "Kasterborous";
+
+$contact = compact('fname', 'sname', 'planet', 'system', 'constellation');
+
+print_r($contact);
