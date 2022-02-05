71d08678fe6a6799f61c3471ca991b372fe2941e
NikitaLejnev <catassasin331@gmail.com>
2022-02-05 10:18:01 +0300

    Bugfixes

--- Chapter10/query-mysqli.php
+++ Chapter10/query-mysqli.php
@@ -1,4 +1,5 @@
 // Fetching results one cell at a time
+<?php
 require_once 'login.php';
 $connection = new mysqli($hn, $un, $pw, $db);
 
@@ -11,27 +12,28 @@ if (!$result) die("Fatal Error");
 
 $rows = $result->num_rows;
 
-for ($j = 0; $j < $rows; ++$j) { $result->data_seek($j);
+for ($j = 0; $j < $rows; ++$j) {
+  $result->data_seek($j);
   echo 'Author: '
-  . htmlspecialchars($result->fetch_assoc()['author'])
-  . '<br>';
+    . htmlspecialchars($result->fetch_assoc()['author'])
+    . '<br>';
   $result->data_seek($j);
   echo 'Title: '
-  . htmlspecialchars($result->fetch_assoc()['title'])
-  . '<br>';
+    . htmlspecialchars($result->fetch_assoc()['title'])
+    . '<br>';
   $result->data_seek($j);
   echo 'Category: '
-  . htmlspecialchars($result->fetch_assoc()['category'])
-  . '<br>';
+    . htmlspecialchars($result->fetch_assoc()['category'])
+    . '<br>';
   $result->data_seek($j);
   echo 'Year: '
-  . htmlspecialchars($result->fetch_assoc()['year'])
-  . '<br>';
+    . htmlspecialchars($result->fetch_assoc()['year'])
+    . '<br>';
   $result->data_seek($j);
   echo 'ISBN: '
-  . htmlspecialchars($result->fetch_assoc()['isbn'])
-  . '<br><br>';
-  }
+    . htmlspecialchars($result->fetch_assoc()['isbn'])
+    . '<br><br>';
+}
 
-  $result->close();
-  $connection->close();
+$result->close();
+$connection->close();
