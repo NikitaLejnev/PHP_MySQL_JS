1f85e20df50dbf27ceadee20535a22852e6d04a1
NikitaLejnev <catassasin331@gmail.com>
2022-02-07 11:04:09 +0300

    Added formtest.php

---
+++ Chapter11/formtest.php
@@ -0,0 +1,16 @@
+// Example 11-1. simple PHP form handler
+<?php
+echo <<<_END
+    <html>
+      <head>
+        <title>Form Test</title>
+      </head>
+    <body>
+    <form method="post" action="formtest.php">
+      What is your name?
+      <input type="text" name="name">
+      <input type="submit">
+    </form>
+    </body>
+  </html>
+_END;
